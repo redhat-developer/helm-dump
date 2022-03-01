5420f7acf9f35a5d63f1a6d23054832d447cdda4
Igor Sutton <isuttonl@redhat.com>
2022-03-01 16:21:38 +0100

    chore: produce plugin tarball for release

--- hack/build-plugin.sh
+++ hack/build-plugin.sh
@@ -8,4 +8,7 @@ mkdir -p "${PLUGIN_BUILD_DIR}"
 cp -R "${DIST_DIR}"/helm-dump_*/ "${PLUGIN_BUILD_DIR}"
 cp ./plugin.yaml "${PLUGIN_BUILD_DIR}"
 yq ".version |= $(yq .version dist/metadata.json)" ./plugin.yaml > "${PLUGIN_BUILD_DIR}/plugin.yaml"
+
+echo "Building tarball..."
+tar -cjf dist/helm-dump.tar.gz -C ./dist/plugin .
 echo "Done!"
