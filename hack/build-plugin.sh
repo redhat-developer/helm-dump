c6fee9c7589208314ae36790a186c03f6a53fc29
Igor Sutton <isuttonl@redhat.com>
2022-03-01 14:28:54 +0100

    chore: propagate release version to plugin file

--- hack/build-plugin.sh
+++ hack/build-plugin.sh
@@ -7,5 +7,5 @@ echo "Building plugin in ${PLUGIN_BUILD_DIR}... "
 mkdir -p "${PLUGIN_BUILD_DIR}"
 cp -R "${DIST_DIR}"/helm-dump_*/ "${PLUGIN_BUILD_DIR}"
 cp ./plugin.yaml "${PLUGIN_BUILD_DIR}"
-
+yq ".version |= $(yq .version dist/metadata.json)" ./plugin.yaml > "${PLUGIN_BUILD_DIR}/plugin.yaml"
 echo "Done!"
