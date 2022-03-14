17415e1212413a3e80a3dea2fade23c244cb9321
Igor Sutton <isuttol@redhat.com>
2022-03-14 17:52:42 +0100

    refactor: decouple init command from dynamic and discovery clients

--- cmd/init.go
+++ cmd/init.go
@@ -3,18 +3,19 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"github.com/vmware-tanzu/velero/pkg/discovery"
 	"helm.sh/helm/v3/pkg/chartutil"
 	"path"
 	"strings"
 
 	"github.com/ghodss/yaml"
-	"github.com/vmware-tanzu/velero/pkg/discovery"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	kdiscovery "k8s.io/client-go/discovery"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/tools/pager"
 
@@ -23,120 +24,120 @@ import (
 	"helm.sh/helm/v3/pkg/chart"
 )
 
-func NewInitCmd(configFlags *genericclioptions.ConfigFlags, dynamicClient dynamic.Interface) *cobra.Command {
+type InitCommand struct {
+	*cobra.Command
+	Logger          *logrus.Logger
+	DynamicClient   dynamic.Interface
+	DiscoveryClient kdiscovery.DiscoveryInterface
+	DiscoveryHelper discovery.Helper
+	ConfigFlags     *genericclioptions.ConfigFlags
+}
 
-	var initCmd = &cobra.Command{
-		Use:   "init chart-name output-dir",
-		Short: "generates a Helm chart from existing resources",
-		Long:  `Generates a Helm chart from existing resources`,
-		Args:  cobra.ExactArgs(2),
-		RunE:  buildInitCmd(configFlags, dynamicClient),
+func NewInitCmd(configFlags *genericclioptions.ConfigFlags, dynamicClient dynamic.Interface, discoveryClient kdiscovery.DiscoveryInterface, logger *logrus.Logger) (*InitCommand, error) {
+	discoveryHelper, err := discovery.NewHelper(discoveryClient, logger)
+	if err != nil {
+		return nil, err
 	}
+
+	initCmd := &InitCommand{
+		Command: &cobra.Command{
+			Use:   "init chart-name output-dir",
+			Short: "generates a Helm chart from existing resources",
+			Long:  `Generates a Helm chart from existing resources`,
+			Args:  cobra.ExactArgs(2),
+		},
+		Logger:          logger,
+		DynamicClient:   dynamicClient,
+		DiscoveryClient: discoveryClient,
+		DiscoveryHelper: discoveryHelper,
+		ConfigFlags:     configFlags,
+	}
+	initCmd.Command.RunE = initCmd.runE
 
 	configFlags.AddFlags(initCmd.Flags())
 
-	return initCmd
+	return initCmd, nil
 }
 
-func buildInitCmd(
-	configFlags *genericclioptions.ConfigFlags,
-	dynamicClient dynamic.Interface,
-) func(cmd *cobra.Command, args []string) error {
-	return func(cmd *cobra.Command, args []string) error {
+func (c *InitCommand) runE(cmd *cobra.Command, args []string) error {
+	chartFiles := make([]*chart.File, 0)
 
-		log := logrus.New()
-		log.Level = logrus.DebugLevel
-
-		discoveryClient, err := configFlags.ToDiscoveryClient()
+	apiResourceLists := c.DiscoveryHelper.Resources()
+	for _, resourceList := range apiResourceLists {
+		gv, err := schema.ParseGroupVersion(resourceList.GroupVersion)
 		if err != nil {
-			return err
+			continue
 		}
-
-		discoveryClient.Invalidate()
-
-		discoveryHelper, err := discovery.NewHelper(discoveryClient, log)
-		if err != nil {
-			return err
-		}
-
-		chartFiles := make([]*chart.File, 0)
-
-		apiResourceLists := discoveryHelper.Resources()
-		for _, resourceList := range apiResourceLists {
-			gv, err := schema.ParseGroupVersion(resourceList.GroupVersion)
-			if err != nil {
+		c.Logger.Debugf("Collecting definitions for %s", gv.String())
+		for _, resource := range resourceList.APIResources {
+			if !resource.Namespaced {
 				continue
 			}
-			log.Debugf("Collecting definitions for %s", gv.String())
-			for _, resource := range resourceList.APIResources {
-				if !resource.Namespaced {
-					continue
-				}
-				log.Debugf("\t%s.%s", gv.String(), resource.Kind)
-				gvr := &schema.GroupVersionResource{
-					Group:    gv.Group,
-					Version:  gv.Version,
-					Resource: resource.Name,
-				}
-				resourceInterface := dynamicClient.Resource(*gvr)
+			c.Logger.Debugf("\t%s.%s", gv.String(), resource.Kind)
+			gvr := &schema.GroupVersionResource{
+				Group:    gv.Group,
+				Version:  gv.Version,
+				Resource: resource.Name,
+			}
+			resourceInterface := c.DynamicClient.Resource(*gvr)
 
-				p := pager.New(func(ctx context.Context, opts metav1.ListOptions) (runtime.Object, error) {
-					return resourceInterface.Namespace(*configFlags.Namespace).List(ctx, opts)
-				})
+			p := pager.New(func(ctx context.Context, opts metav1.ListOptions) (runtime.Object, error) {
+				return resourceInterface.Namespace(*c.ConfigFlags.Namespace).List(ctx, opts)
+			})
 
-				list, _, err := p.List(cmd.Context(), metav1.ListOptions{})
+			list, _, err := p.List(cmd.Context(), metav1.ListOptions{})
+			if err != nil {
+				c.Logger.Errorf("%s", err)
+				continue
+			}
+
+			err = meta.EachListItem(list, func(object runtime.Object) error {
+				u, ok := object.(*unstructured.Unstructured)
+				if !ok {
+					return fmt.Errorf("expected *unstructured.Unstructured but got #{u}")
+				}
+
+				name := nameFromUnstructured(u)
+				name = path.Join("templates", name)
+				file, err := fileFromUnstructured(u, name)
 				if err != nil {
-					log.Errorf("%s", err)
-					continue
+					return err
 				}
 
-				err = meta.EachListItem(list, func(object runtime.Object) error {
-					u, ok := object.(*unstructured.Unstructured)
-					if !ok {
-						return fmt.Errorf("expected *unstructured.Unstructured but got #{u}")
-					}
+				chartFiles = append(chartFiles, file)
 
-					name := nameFromUnstructured(u)
-					name = path.Join("templates", name)
-					file, err := fileFromUnstructured(u, name)
-					if err != nil {
-						return err
-					}
-
-					chartFiles = append(chartFiles, file)
-
-					return nil
-				})
-				if err != nil {
-					log.Errorf("%s", err)
-				}
+				return nil
+			})
+			if err != nil {
+				c.Logger.Errorf("%s", err)
 			}
 		}
-
-		for _, chartFile := range chartFiles {
-			log.Debugf("name: %s\ndata:\n%s", chartFile.Name, string(chartFile.Data))
-		}
-
-		name := args[0]
-		chrt := &chart.Chart{
-			Metadata: &chart.Metadata{
-				APIVersion: "v2",
-				Name:       name,
-				Version:    "0.1.0",
-			},
-			Files: chartFiles,
-		}
-
-		outDir := args[1]
-		save, err := chartutil.Save(chrt, outDir)
-		if err != nil {
-			return err
-		}
-
-		log.Debugf("chart stored in %s", save)
-
-		return nil
 	}
+
+	for _, chartFile := range chartFiles {
+		c.Logger.Debugf("name: %s\ndata:\n%s", chartFile.Name, string(chartFile.Data))
+	}
+
+	name := args[0]
+	chrt := &chart.Chart{
+		Metadata: &chart.Metadata{
+			APIVersion: "v2",
+			Name:       name,
+			Version:    "0.1.0",
+		},
+		Files: chartFiles,
+	}
+
+	outDir := args[1]
+	save, err := chartutil.Save(chrt, outDir)
+	if err != nil {
+		return err
+	}
+
+	c.Logger.Debugf("chart stored in %s", save)
+
+	return nil
+
 }
 
 func fileFromUnstructured(obj *unstructured.Unstructured, name string) (*chart.File, error) {
@@ -170,6 +171,9 @@ func nameFromUnstructured(obj *unstructured.Unstructured) string {
 }
 
 func init() {
+	logger := logrus.New()
+	logger.Level = logrus.DebugLevel
+
 	configFlags := genericclioptions.NewConfigFlags(true)
 	restConfig, err := configFlags.ToRESTConfig()
 	if err != nil {
@@ -177,5 +181,17 @@ func init() {
 	}
 
 	dynamicClient := dynamic.NewForConfigOrDie(restConfig)
-	rootCmd.AddCommand(NewInitCmd(configFlags, dynamicClient))
+	discoveryClient, err := configFlags.ToDiscoveryClient()
+	if err != nil {
+		panic(err)
+	}
+
+	discoveryClient.Invalidate()
+
+	initCmd, err := NewInitCmd(configFlags, dynamicClient, discoveryClient, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	rootCmd.AddCommand(initCmd.Command)
 }
--- cmd/init_test.go
+++ cmd/init_test.go
@@ -2,55 +2,63 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/stretchr/testify/require"
 	"helm.sh/helm/v3/pkg/chart/loader"
 	"io/ioutil"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/rest"
+	fakediscovery "k8s.io/client-go/discovery/fake"
+	fakedynamic "k8s.io/client-go/dynamic/fake"
 	"os"
 	"path"
 	"testing"
 )
 
-func buildConfigFlagsAndRestConfig() (*genericclioptions.ConfigFlags, *rest.Config) {
-	configFlags := genericclioptions.NewConfigFlags(true)
-	restConfig, err := configFlags.ToRESTConfig()
-	if err != nil {
-		panic(err)
+func buildClients() (*fakedynamic.FakeDynamicClient, *fakediscovery.FakeDiscovery) {
+	scheme := runtime.NewScheme()
+	dynamicClient := fakedynamic.NewSimpleDynamicClient(scheme)
+	discoveryClient := &fakediscovery.FakeDiscovery{
+		Fake: &dynamicClient.Fake,
 	}
-	return configFlags, restConfig
+
+	return dynamicClient, discoveryClient
 }
 
 func TestNewInitCmd(t *testing.T) {
-	t.Run("no-arguments", func(t *testing.T) {
-		configFlags, restConfig := buildConfigFlagsAndRestConfig()
-		dynamicClient := dynamic.NewForConfigOrDie(restConfig)
-		cmd := NewInitCmd(configFlags, dynamicClient)
+	logger, _ := test.NewNullLogger()
 
+	t.Run("no-arguments", func(t *testing.T) {
+		// Arrange
+		dynamicClient, discoveryClient := buildClients()
+
+		configFlags := genericclioptions.NewConfigFlags(true)
+		cmd, _ := NewInitCmd(configFlags, dynamicClient, discoveryClient, logger)
 		cmd.SetArgs([]string{})
 
+		// Act & Assert
 		require.Error(t, cmd.Execute(), "Cmd requires an output directory")
 	})
 
 	t.Run("minimum-required-arguments", func(t *testing.T) {
+		// Arrange
 		chartName := "my-chart"
 		chartVersion := "0.1.0"
 
 		outDir, err := ioutil.TempDir(os.TempDir(), "helm-dump")
 		require.NoError(t, err, "temp directory is required for testing")
 
-		configFlags, restConfig := buildConfigFlagsAndRestConfig()
-		dynamicClient := dynamic.NewForConfigOrDie(restConfig)
-		cmd := NewInitCmd(configFlags, dynamicClient)
-
-		// helm dump init chart-dir
+		dynamicClient, discoveryClient := buildClients()
+		configFlags := genericclioptions.NewConfigFlags(true)
+		cmd, _ := NewInitCmd(configFlags, dynamicClient, discoveryClient, logger)
 		cmd.SetArgs([]string{
 			"--namespace", "default",
 			chartName, outDir})
 
+		// Act
 		require.NoError(t, cmd.Execute(), "Cmd must not return an error")
 
+		// Assert
 		expectedChartPath := path.Join(outDir, fmt.Sprintf("%s-%s.tgz", chartName, chartVersion))
 		_, err = os.Stat(expectedChartPath)
 		require.NoError(t, err, "%q should exist", expectedChartPath)
